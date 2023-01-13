a8a2fdb4fb96dc6227e64b9df07affa949c2adcb
eddyK15501 <edk1231@gmail.com>
2023-01-13 00:47:06 -0500

    gh-pages update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Blog from "./pages/Blog/Blog";
 import Resume from './pages/Resume';
